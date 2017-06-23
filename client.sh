11a1e14e5325150308f60962ea440000036c03e7
Jason Sun <sunapi386@gmail.com>
2017-06-23 08:57:11 -0400

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "$# arguments"
+if [ $# -ne 1 ]; then 
+	echo "illegal number of parameters"
+	exit -1
+fi
+
+# Generate a client certificate
+export CLIENTNAME="$1"
+
+echo "Generating client certificate: " $CLIENTNAME
+
+# with a passphrase (recommended)
+docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME
+# without a passphrase (not recommended)
+# docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME nopass
+# Retrieve the client configuration with embedded certificates
+docker-compose run --rm openvpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
+
+echo "Complete"
+echo "$CLIENTNAME.ovpn"
